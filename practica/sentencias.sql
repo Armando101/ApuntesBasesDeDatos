34bb90c8cfd44c033403cbadde68e98b7917a121
Armando <rivera.armando997@gmail.com>
2019-07-28 17:53:15 -0500

    Sentencias básicas

--- practica/sentencias.sql
+++ practica/sentencias.sql
@@ -37,7 +37,7 @@ CREATE TABLE IF NOT EXISTS libros (
 	paginas INT UNSIGNED,
 	fecha_publicacion DATE NOT NULL,
 	fecha_creacion DATETIME DEFAULT current_timestamp,
-	FOREIGN KEY(autor_id) REFERENCES autores(autor_id)
+	FOREIGN KEY(autor_id) REFERENCES autores(autor_id) ON DELETE CASCADE
 );
 
 /* Insercción de autores*/
@@ -52,7 +52,8 @@ INSERT INTO autores(nombre, apellido, pseudonimo, genero, fecha_nacimiento, pais
 INSERT INTO libros(autor_id, titulo, fecha_publicacion)
 	VALUES (1, 'Carrie', '1974-01-01'),
 			(1, 'El misterio de Salems Lot', '1975-01-01'),
-			(1, 'El resplandor', '1977-01-01'),
+			(1, 'Ojos de fuego', '1980-01-01'),
+			(1, 'Cujo', '1981-01-01'),
 
 			(2, 'Harry Potter y la piedra filosofal', '1997-06-30'),
 			(2, 'Harry Potter y la camara secreta', '1998-07-2'),
@@ -62,6 +63,6 @@ INSERT INTO libros(autor_id, titulo, fecha_publicacion)
 			(2, 'Harry Potter y el misterio del príncipe', '2005-06-16'),
 			(2, 'Harry Potter y las reliquias de la muerte', '2007-07-21');
 
-
+ALTER TABLE libros ADD ventas INT UNSIGNED NOT NULL;
 SELECT * FROM autores;
 SELECT * FROM libros;
